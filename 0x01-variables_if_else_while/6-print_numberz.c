3e2ec061db57bb612954d21d23f3c9a894e26a2f
Alaa7318 <lololula1@hotmail.com>
2023-09-08 18:53:33 -0700

    kkk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+{
+	putchar(a + 48);
+}
+putchar('\n');
+return (0);
+}
