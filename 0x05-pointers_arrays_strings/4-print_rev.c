59e2444559f13920b76e64ea56ce586b35bd91df
Alaa7318 <lololula1@hotmail.com>
2023-09-18 16:13:36 -0700

    kkk

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,12 @@
 
 void print_rev(char *s)
 {
-int i;
-int count = 0;
-for (i = 0; s[i] != '\0'; i++)
-	count++;
-for (i = count - 1; i >= 0; i--)
-	_putchar(s[i]);
-	_putchar('\n');
+int i = 0;
+while (s[i])
+	i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
