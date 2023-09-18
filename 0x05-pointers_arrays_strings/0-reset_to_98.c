d5eda184f499efa7bd033f88b26d24bac2151cf4
Alaa7318 <lololula1@hotmail.com>
2023-09-18 14:51:32 -0700

    kkk

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * this_the_firist - takes a pointer to an int parameter
+ *	and updates the value it point to 98
+ * @n: int parameter
+ *
+ * Returnn: Nothing
+*/
+
+void this_the_firist(int *n)
+{
+	*n = 98;
+}
